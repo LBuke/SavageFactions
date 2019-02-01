33585c8d7113028a369d01a88601cd587e12d196
Teddeh <teddeh@grandtheftmc.net>
2019-02-01 16:30:33 +0000

    Array of fac members online placeholder.

--- src/main/java/com/massivecraft/factions/scoreboards/sidebar/FDefaultSidebar.java
+++ src/main/java/com/massivecraft/factions/scoreboards/sidebar/FDefaultSidebar.java
@@ -3,6 +3,9 @@ package com.massivecraft.factions.scoreboards.sidebar;
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.SavageFactions;
 import com.massivecraft.factions.scoreboards.FSidebarProvider;
+import com.massivecraft.factions.struct.Role;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,10 +35,54 @@ public class FDefaultSidebar extends FSidebarProvider {
             return new ArrayList<>();
         }
 
+        for (String str : lines) {
+//            System.out.println(str);
+            if (!str.equalsIgnoreCase("<facmemberlist>"))
+                continue;
+
+            int index = 1;
+            for (Role role : Role.values()) {
+                for (FPlayer fp : fplayer.getFaction().getFPlayersWhereRole(role)) {
+                    if (!fp.isOnline())
+                        continue;
+
+                    if (index >= 10)
+                        break;
+
+                    lines.add(ChatColor.GREEN + fp.getNameAndTitle() + " " + getHealth(fp.getPlayer()));
+                    index += 1;
+                }
+            }
+
+            break;
+        }
+
         ListIterator<String> it = lines.listIterator();
         while (it.hasNext()) {
-            it.set(replaceTags(fplayer, it.next()));
+            String original = it.next();
+            String str = replaceTags(fplayer, original);
+
+            if (original.equalsIgnoreCase("<facmemberlist>")) {
+                it.remove();
+                continue;
+            }
+
+            it.set(str);
         }
         return lines;
+    }
+
+    private String getHealth(Player player) {
+        String str = "";
+        if (player.getHealth() > 13)
+            str = ChatColor.GREEN.toString() + (int) player.getHealth();
+
+        else if (player.getHealth() > 7)
+            str = ChatColor.GOLD.toString() + (int) player.getHealth();
+
+        else
+            str = ChatColor.RED.toString() + (int) player.getHealth();
+
+        return ChatColor.WHITE + "(" + str + ChatColor.WHITE + ")";
     }
 }
